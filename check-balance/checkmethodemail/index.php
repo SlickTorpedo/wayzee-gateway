348d89b5a234712365e438c5656eb1e7cbf013c9
Slick Torpedo <55722867+SlickTorpedo@users.noreply.github.com>
2022-03-13 11:03:33 -0700

    Create index.php

---
+++ check-balance/checkmethodemail/index.php
@@ -0,0 +1,184 @@
+<!DOCTYPE html>
+<?php
+mysqli_close($conn);
+
+$servername = "0.0.0.0";
+$username = "username";
+$password = "password";
+$dbname = "panel";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+$email = mysqli_real_escape_string($conn, $_GET["email"]);
+$sql = "SELECT * FROM users WHERE email = '$email';";
+$result = mysqli_query($conn, $sql);
+
+
+if (mysqli_num_rows($result) > 0) {
+  // output data of each row
+  while($row = mysqli_fetch_assoc($result)) {
+    $emaildb = $row["id"]. "<br>";
+  }
+} else {
+  $resultssearch = "Error";
+}
+
+mysqli_close($conn);
+
+
+// CHECK FOR USER ID'S CREDITS
+
+$servername = "0.0.0.0";
+$username = "username";
+$password = "password";
+$dbname = "panel";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+$id = $emaildb;
+$sql = "SELECT * FROM `billing_users` WHERE `user_id` = '$id'";
+$result = mysqli_query($conn, $sql);
+
+
+if (mysqli_num_rows($result) > 0) {
+  // output data of each row
+  while($row = mysqli_fetch_assoc($result)) {
+    $resultssearch = $row["balance"]. "<br>";
+  }
+} else {
+  $resultssearch = "Sorry, we couldn't find your Email!";
+}
+?>
+<html>
+
+<head>
+  <!-- Basic -->
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <!-- Mobile Metas -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <!-- Site Metas -->
+  <meta name="keywords" content="" />
+  <meta name="description" content="" />
+  <meta name="author" content="" />
+
+  <title>Nexus Hosting - Check Balance</title>
+
+  <!-- slider stylesheet -->
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.1.3/assets/owl.carousel.min.css" />
+
+  <!-- bootstrap core css -->
+  <link rel="stylesheet" type="text/css" href="https://nexussociety.net/css/bootstrap.css" />
+
+  <!-- fonts style -->
+  <link href="https://fonts.googleapis.com/css?family=Poppins:400,600,700&display=swap" rel="stylesheet">
+  <!-- Custom styles for this template -->
+  <link href="https://nexussociety.net/css/style.css" rel="stylesheet" />
+  <!-- responsive style -->
+  <link href="https://nexussociety.net/css/responsive.css" rel="stylesheet" />
+</head>
+
+<body class="sub_page">
+
+  <div class="hero_area">
+    <!-- header section strats -->
+    <header class="header_section">
+      <div class="container">
+        <nav class="navbar navbar-expand-lg custom_nav-container ">
+          <a class="navbar-brand" href="https://nexussociety.net">
+            <span>
+              Credits
+            </span>
+          </a>
+          </a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="s-1"> </span>
+            <span class="s-2"> </span>
+            <span class="s-3"> </span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <div class="d-flex ml-auto flex-column flex-lg-row align-items-center">
+              <ul class="navbar-nav  ">
+                <li class="nav-item ">
+                  <a class="nav-link" href="https://nexussociety.net">Home <span class="sr-only">(current)</span></a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </header>
+    <!-- end header section -->
+  </div>
+
+
+  <!-- service section -->
+  <section class="service_section layout_padding">
+    <div class="container">
+      <div class="heading_container">
+        <h2>
+          Check Credits
+        </h2>
+      </div>
+
+      <div class="service_container">
+        <div class="box active">
+          <div class="img-box">
+            <img src="https://nexussociety.net/payment-test/imhg/money-bag.png" class="img1" alt="">
+            <img src="https://nexussociety.net/payment-test/imhg/money-bag.png" class="img2" alt="">
+          </div>
+          <div class="detail-box">
+            <h5>
+              <strong><?php echo $resultssearch ?></strong>
+            </h5>
+          </div>
+        </div>
+      </div>
+      <div class="btn-box">
+        <a href="https://panel.nexussociety.net">
+          I'd like to use those now!
+        </a>
+      </div>
+    </div>
+  </section>
+  <!-- end service section -->
+
+  <div class="footer_bg">
+
+
+    <!-- footer section -->
+    <section class="container-fluid footer_section">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-7 col-md-9 mx-auto">
+            <p>
+              &copy; 2022 All Rights Reserved By
+              <a href="https://nexussociety.net/">NexusSociety</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- footer section -->
+
+  </div>
+
+
+
+  <script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
+  <script type="text/javascript" src="js/bootstrap.js"></script>
+
+</body>
+
+</html>
