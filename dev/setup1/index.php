2335b229fcfb60af8125cf1c7be34c48cffa4d37
Slick Torpedo <55722867+SlickTorpedo@users.noreply.github.com>
2022-03-17 14:27:39 -0700

    Update index.php

--- dev/setup1/index.php
+++ dev/setup1/index.php
@@ -2,7 +2,7 @@
 
 $sitename = htmlspecialchars($_GET['sitename']);
 
-$txt = Name: $sitename\n;
+$txt = "Name: $sitename\n";
 
 $myfile = fopen("config.txt", "w") or die("Unable to open / generate config file!");
 fwrite($myfile, $txt);
