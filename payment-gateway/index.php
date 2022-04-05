83c8f7fb536f2f37588b9045082a8b8871a6adc1
Slick Torpedo <55722867+SlickTorpedo@users.noreply.github.com>
2022-04-05 12:52:57 -0500

    Update index.php

--- payment-gateway/index.php
+++ payment-gateway/index.php
@@ -236,8 +236,8 @@
 
 
 
-  <script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
-  <script type="text/javascript" src="js/bootstrap.js"></script>
+  <script type="text/javascript" src="https://nexussociety.net/js/jquery-3.4.1.min.js"></script>
+  <script type="text/javascript" src="https://nexussociety.net/js/bootstrap.js"></script>
 
 </body>
 
